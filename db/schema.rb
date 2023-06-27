19aec4dc943fccb06255223a11a1d03eff16f8ef
chanoir <chanoir@chanoirs-MBP.fritz.box>
2023-06-27 01:26:31 +0100

    feat: add field for ip addresses

--- db/schema.rb
+++ db/schema.rb
@@ -73,6 +73,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_25_220808) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.integer "role", default: 0
