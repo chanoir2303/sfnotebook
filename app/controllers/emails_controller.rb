72dd0a42065f8f4089def082e774b44d54af73a9
chanoir <chanoir@chanoirs-MBP.fritz.box>
2023-07-08 10:24:09 +0100

    feat: navbar

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,6 +2,11 @@
 
 # Handle emails registration. All Email belongs to a Person.
 class EmailsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @emails = Email.all
+  end
   def create
     @person = Person.find(params[:person_id])
     @email = @person.emails.create(email_params)
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
     resources :networks
     resources :emails
   end
+  resources :emails
+  resources :networks
 end
