ff8a4df6cc4ad000b80fd7236d645b4d6cc9c8aa
chanoir <chanoir@chanoirs-MBP.fritz.box>
2023-07-13 00:08:02 +0100

    fix: docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-# If the database exists, migrate. Otherwise setup (create and migrate)
-bundle exec rake db:reset && bundle exec rake db:migrate || bundle exec rake db:setup
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
+
+# then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
